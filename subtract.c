ba0bd3dbfb721a053b9b6e20e1391d77b06bebe3
Your Name <you@example.com>
2023-02-14 15:56:37 +0530

    cprogramming

---
+++ subtract.c
@@ -0,0 +1,12 @@
+//wap to substracting by using user input
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter a and b");
+scanf("%d %d",&a,&b);
+int sub=a-b;
+printf("%d",sub);
+return 0;
+
+}
